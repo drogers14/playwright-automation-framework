1c8567d45efaafb0ecb946352e000e9ae34f1b40
Destiny Rogers <destinyrogers725@gmail.com>
2026-05-15 19:26:14 -0700

    building folder hierarchy

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # playwright-automation-framework
 a Demo Playwright Automation Framework to support SauceDemo testing
+
+*notes*
+homepage
+product list page
+product desc page
+about us
+search 
+sign up
+log in
+cart
--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -6,3 +6,13 @@ test('has title', async ({ page }) => {
     // Expect a title "to contain" a substring.
     await expect(page).toHaveTitle(/Sauce Demo/);
 });
+
+test('get catalog link', async ({ page }) => {
+    await page.goto('https://sauce-demo.myshopify.com/');
+  
+    // Click the catalog link.
+    await page.getByRole('link', { name: 'Catalog' }).click();
+  
+    // Expects page to have a heading with the name of Catalog.
+    await expect(page.getByRole('heading', { name: 'Products' })).toBeVisible();
+  });
