50be8edcf2e3ee268fc9aa99f038456ac447323e
Destiny Rogers <destinyrogers725@gmail.com>
2026-05-17 20:25:22 -0700

    added to cart test naviagates to pdp

--- tests/Cart/cart.spec.ts
+++ tests/Cart/cart.spec.ts
@@ -7,8 +7,11 @@ test('add item to cart', async ({ page }, testInfo) =>{
     
     // Expect a title "to contain" a substring.
     await expect(page).toHaveTitle(/Sauce Demo/);
-    // const product = 
-    await page.locator('#product-1').click();
+    const product = page.locator('#product-1');
+    await product.click();
+    // Expects page to have a heading with the name of Grey jacket.
+    await expect(page.getByRole('heading', { name: 'Grey jacket' })).toBeVisible();
+
     const screenshot = await page.screenshot();
     await testInfo.attach('screenshot', { body: screenshot, contentType: 'image/png' });
 
